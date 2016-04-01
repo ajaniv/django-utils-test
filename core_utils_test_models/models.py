c54c654e750f53c7a42c9dbacc282fb13f71c713
Amnon Janiv <amnon.janiv@ondalear.com>
2016-04-01 16:56:45 -0400

    Increase unit test coverage

--- core_utils_test_models/models.py
+++ core_utils_test_models/models.py
@@ -6,8 +6,9 @@ utils_test models module.
 
 """
 from __future__ import absolute_import
-from core_utils.models import db_table
-from core_utils.models import VersionedModel
+
+from core_utils.models import (NamedModel, PrioritizedModel, VersionedModel,
+                               db_table)
 
 from .apps import CoreUtilsTestModelsConfig
 
@@ -15,6 +16,30 @@ _app_label = CoreUtilsTestModelsConfig.name
 
 
 class MyVersionedModel(VersionedModel):
+    """
+    Sample versioned model class.
+    """
     class Meta(object):
+        """Model meta class."""
         app_label = _app_label
         db_table = db_table(_app_label, "MyVersionedModel")
+
+
+class MyNamedModel(NamedModel):
+    """
+    Sample named  model class.
+    """
+    class Meta(object):
+        """Model meta class."""
+        app_label = _app_label
+        db_table = db_table(_app_label, "MyNamedModel")
+
+
+class MyPrioritizedModel(VersionedModel, PrioritizedModel):
+    """
+    Sample prioritized model class.
+    """
+    class Meta(object):
+        """Model meta class."""
+        app_label = _app_label
+        db_table = db_table(_app_label, "MyPrioritizedModel")
