a1ecfe559bcdb845390638ac9c7050519eec1525
Amnon Janiv <amnon.janiv@ondalear.com>
2018-09-13 14:38:58 -0400

    invalid 2.7 exception handling

--- manage.py
+++ manage.py
@@ -13,6 +13,7 @@ if __name__ == '__main__':
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
+        
         if sys.version_info < (3,):
             raise ImportError(msg)
         else:
